c6cf827c3974252d1c25bf92834fe6d7f7892d2b
scottdavenport <scott@pinpoint.com>
2022-09-10 08:59:06 -0400

    setup axios with defaults for youtube API

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyCBn0_zjkaA_jtb7I_5RQkS4MqnyL5HQts";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
